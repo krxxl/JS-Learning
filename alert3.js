643a13d67f4d6851d24430157b4a74b75ce13ab1
krxxl <krrxl9@gmail.com>
2018-11-02 10:08:37 +0400

    add js answer

---
+++ alert3.js
@@ -0,0 +1,5 @@
+var x = prompt('print > 100', '')
+
+while( x < 100 && x != null){
+	x = prompt('print > 100', '');	
+}
---
+++ alert4.js
@@ -0,0 +1,6 @@
+iter: for(var i = 2; i < 10; i++){
+	for(var j = 2; j < i; j++){
+		if (i % j == 0) continue iter;
+	}
+	alert(i)
+}
