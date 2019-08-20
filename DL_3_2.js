89d0fffd0539796c862eb7bcbdc3d1cbe4f2c7f6
krxxl <39261348+krxxl@users.noreply.github.com>
2019-08-20 10:24:54 +0400

    Create DL_3_2.js

---
+++ DL_3_2.js
@@ -0,0 +1,15 @@
+arr = []
+
+for(let i = 0; i < 5; i++){
+  arr[i] = prompt(`arr[${i}]:`,'')	
+}
+
+ for(let i = 4; i >= 0; i--){
+  alert(arr[i]);
+} 
+
+let j = 4;
+while(j>=0){
+  alert(arr[j]);
+  j--;
+}
